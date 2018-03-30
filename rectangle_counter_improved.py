5979232052c41b52f3eb046e438bfe355c4d6784
kkb14183 <kkb14183@uni.strath.ac.uk>
2018-03-30 17:11:57 +0300

    better solution introduced - all incorrect rectangles are removed from a total number of possible rectangles

---
+++ rectangle_counter_improved.py
@@ -0,0 +1,34 @@
+
+rectangles_representation = [[1, 1, 1, 1, 1, 1, 1],
+                             [1, 1, 3, 0, 0, 1, 1],
+                             [1, 1, 1, 1, 1, 1, 1]]
+
+dimension_rows = len(rectangles_representation)
+dimension_cols = len(rectangles_representation[0])
+max_number_of_rectangles_on_grid = dimension_rows*(dimension_rows + 1)*dimension_cols*(dimension_cols+1)/4
+
+def find_num_incorrect_rectangles(matrix):
+    mistake_count = 0
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            try:
+                is_it_shared = False
+                if (matrix[r][c] > 1 or matrix[r][c] == 0) and matrix[r][c+1] == 0:
+                    count=(len(matrix[r][:c+1])*(len(matrix)+1))
+                    mistake_count+=count
+                    is_it_shared = True
+                if matrix[r][c] == 0 and (matrix[r][c-1] == 0 or matrix[r][c-1] > 1):
+                    count = (len(matrix[r][c:])*(len(matrix)+1))
+                    if is_it_shared:
+                        mistake_count+=(count - len(matrix) - 1)
+                    else:
+                        mistake_count+=count
+            #    if (matrix[r][c] > 1 or matrix[r][c] == 0) and matrix[r+1][c] == 0:
+                #    mistake_count+=(len(matrix[r][0])*len(matrix[:r+1]))
+            #    if matrix[r][c] == 0 and (matrix[r-1][c] == 0 or matrix[r-1][c] > 0):
+                #    mistake_count+=(len(matrix[r][0])*len(matrix[r:]))
+            except:
+                pass
+    return mistake_count
+
+print(max_number_of_rectangles_on_grid - find_num_incorrect_rectangles(rectangles_representation))
